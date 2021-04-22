983222688ed805547e0b4baef535712a6e3cd87e
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-04-22 19:37:23 +0530

    Add files via upload

---
+++ MasaiSchool.java
@@ -0,0 +1,9 @@
+package com.company.masaijava;
+
+public class MasaiSchool{
+    public static void main(String[] args){
+        //Question 1 = Print Masai School on the console
+      System.out.println("hello masai");
+    }
+
+        }
