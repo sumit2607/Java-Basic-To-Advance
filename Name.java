a8cab37a8e14a5d23b4618a9d64c4a6a77a82975
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-04-22 19:42:22 +0530

    Update Name.java
    //Create a variable called name use it to print your father's name and your name

--- Name.java
+++ Name.java
@@ -3,7 +3,7 @@ package com.company.masaijava;
 public class Name
 {
     public static void main(String[] args){
-        //Create a variable called name use it to print your father's name and your name
+        //Create a variable called name use it to print your father's name and your name  
         String name = "sumit";
         System.out.println("My name is " + name);
         name = "xyz";
