aa4faa9fddff3a90ac2e6cc2d610c6ae3e996445
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-04-22 19:44:36 +0530

    DataTypeAndVariable

---
+++ Allname.java
@@ -0,0 +1,16 @@
+package upart;
+
+public class Allname {
+    public static void main(String[] args) {
+       // Create a variable called name use it to print your name, your father's name and your mother's name
+
+        String name = "sumit";
+        System.out.println("My name is " + name);
+
+        name = "sonu";
+        System.out.println("My father name " + name);
+
+        name = "sunita";
+        System.out.println("My mother name " + name);
+    }
+}
---
+++ Masai.java
@@ -0,0 +1,11 @@
+package upart;
+
+public class Masai {
+    public static void main(String[] args) {
+       // Print Masai School and Welcome on the console in two different lines
+
+        System.out.print("hello Masai ");
+        System.out.println("Welcome");
+
+    }
+}
