6c22d35a58b39c6505a1cc05c271df2ced8be88d
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-04-22 19:38:45 +0530

    DataTypeAndVariable

---
+++ Age.java
@@ -0,0 +1,9 @@
+package com.company.masaijava;
+
+public class Age {
+    public static void main(String[] args) {
+        //question Print your name by it storing in a variable
+        int age = 23;
+        System.out.println(age);
+    }
+}
